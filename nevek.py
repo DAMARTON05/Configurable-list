c14b2f99399e6202c59f8f6450e3511640408294
DAMARTON05 <damarton05@gmail.com>
2023-03-29 21:38:51 +0200

    In this branch I made the list configureable with add possible remove, insert and sort

--- nevek.py
+++ nevek.py
@@ -16,36 +16,68 @@ print(nevekSz)"""
 
 
 nevek1 = []
+exit = False
 
-runn = int(input("Approximately how many names will be written? "))
-runn *= 1.5
-x = input("\033[1;32mName: \033[1;37m")
+runn = int(input("\033[1;32mHow many names will be there max?  "))
+print("Then, you can write maximum", runn, "names.\033[1;37m")
+
+x = input("\033[1;34mName: \033[1;37m")
 nevek1.append(x.title())
 
-for i in range(runn):
-    x = input("\033[1;32mNext name or exit: \033[1;37m")
+while exit != True and runn >= len(nevek1):
+    x = input("\033[1;34mNext name or exit: \033[1;37m")
     nevek1.append(x.title())
     if 'Exit' in nevek1:
         nevek1.remove("Exit")
         answer = input("\033[1;31mNo other names wanted(true/false): \033[1;37m")
         if answer.lower() == "true":
-            break
+            exit = True
         elif answer.lower() == "false":
             print("\033[1;31m\033[1;4mEXIT\033[0m isn't compatible with this list.")
         else:
             print("\033[1;31mYou typed something wrong.\033[1;37m")
 
-check = input("\033[1;35mDo you want to check your list(yes/no)? \033[1;37m")
-if check.lower() == "yes":
-    print("Your list:", nevek1)
-    check2 = input("Everything's all right(yes/no)? ")
-    if check2.lower() == "yes":
-        pass
-    elif check2.lower() == "no":
-        pass
-elif check.lower() == "no":
-    print("Passing without checking...")
-else:
-    print("You typed something wrong.")
+passing = False
+
+while passing == False:
+    check = input("\033[1;35mDo you want to check your list(yes/no)? \033[1;37m")
+    if check.lower() == "yes":
+        print("\033[1;33mYour list:\033[1;37m", nevek1)
+        check2 = input("Everything's all right(yes/no)? ")
+        if check2.lower() == "yes":
+            passing = True
+            print("That's good.")
+        elif check2.lower() == "no":
+            passing = True
+            command = ""
+            while command.lower() != 'done':
+                command = input("What kind of changes do you want?(type HELP for the commands.) ")
+                if command.lower() == 'help':
+                    print("remove - remove an element from the list.\ninsert - place a new element in the list\nsort - sort the line alphabetically\ndone - done with changes")
+                elif command.lower() == 'remove':
+                    quantity = input("How many parts you want to remove? ")
+                    for x in range(int(quantity)):
+                        what = input("What do you want to remove? ")
+                        nevek1.remove(what.title())
+                elif command.lower() == 'insert':
+                    quantity = input("How many things you want to insert? ")
+                    for x in range(int(quantity)):
+                        where = input("Where do you want to insert? ")
+                        what = input("What do you want to insert? ")
+                        where -= 1
+                        nevek1.insert(int(where), what)
+                elif command.lower() == 'sort':
+                    nevek1.sort()
+                elif command.lower() == 'done':
+                    print("No other changes will be made.")
+                else:
+                    print("You mistyped something.")
+        else:
+            print("You mistyped something.")
+    elif check.lower() == "no":
+        passing = True
+        print("Passing without checking...")
+    else:
+        print("You typed something wrong.")
 
 
