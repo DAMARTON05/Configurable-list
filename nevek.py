2c5c31e5c365abd3fcd0c8f2d2a448d18ccb2a7d
DAMARTON05 <damarton05@gmail.com>
2023-03-30 17:05:33 +0200

    I added the random number generator to this project, colored the texts and I corrected a few mistakes. In the future I'm planning to make a leaderboard thing.

--- nevek.py
+++ nevek.py
@@ -1,5 +1,6 @@
 
 import random
+import time
 
 """nevek=["Hegedűs Sándor","Dávid Márton","Bojtos Márk","Harangi Márton Balázs","Orlai Zsombor Botond","Oláh Gergő Csongor","Környei Márk István","Czirják Noel","Balogh Kevin","Vitális Viktor","Li Yuanhao","Karsai Zétény","Hecks Márton","Balázs Dávid","Nagy Trisztán","Markus Ádám","Várkonyi Dorisz","Kárász Zsombor","Németh Arion László","Karlovics Tamás","Dömötör Marcell","Nagy Ádám","Tóvizi Réka" ,"Molnár Gergő Boldizsár","Kölcze Szabolcs","Szűcs Roland Balázs","Szabó Zétény","Nagy Boglárka","Lerch Levente","Takács Luca","Molnar Vimce Béla","Mező Gábor"]
 nevek.sort()
@@ -18,13 +19,14 @@ print(nevekSz)"""
 nevek1 = []
 exit = False
 
-runn = int(input("\033[1;32mHow many names will be there max?  "))
-print("Then, you can write maximum", runn, "names.\033[1;37m")
+runn = int(input("\033[1;32mHow many names will be there max? "))
+print("\033[1;32mThen, you can write maximum", "\033[1;4m\033[1;31m" + str(runn) + "\033[1;0m\033[1;37m", "\033[1;32mnames.\033[1;37m")
+runn = int(runn)-1
 
 x = input("\033[1;34mName: \033[1;37m")
 nevek1.append(x.title())
 
-while exit != True and runn >= len(nevek1):
+while exit != True and int(runn) >= len(nevek1):
     x = input("\033[1;34mNext name or exit: \033[1;37m")
     nevek1.append(x.title())
     if 'Exit' in nevek1:
@@ -33,8 +35,10 @@ while exit != True and runn >= len(nevek1):
         if answer.lower() == "true":
             exit = True
         elif answer.lower() == "false":
+            runn += 1
             print("\033[1;31m\033[1;4mEXIT\033[0m isn't compatible with this list.")
         else:
+            runn += 1
             print("\033[1;31mYou typed something wrong.\033[1;37m")
 
 passing = False
@@ -43,41 +47,84 @@ while passing == False:
     check = input("\033[1;35mDo you want to check your list(yes/no)? \033[1;37m")
     if check.lower() == "yes":
         print("\033[1;33mYour list:\033[1;37m", nevek1)
-        check2 = input("Everything's all right(yes/no)? ")
+        check2 = input("\033[1;34mEverything's all right(yes/no)? \033[1;37m")
         if check2.lower() == "yes":
             passing = True
-            print("That's good.")
+            print("\033[1;33mThat's good.\033[1;37m")
         elif check2.lower() == "no":
             passing = True
             command = ""
             while command.lower() != 'done':
-                command = input("What kind of changes do you want?(type HELP for the commands.) ")
+                command = input("\033[1;35mWhat kind of changes do you want?(type HELP for the commands.) \033[1;37m")
                 if command.lower() == 'help':
-                    print("remove - remove an element from the list.\ninsert - place a new element in the list\nsort - sort the line alphabetically\ndone - done with changes")
+                    time.sleep(1)
+                    print("\033[1;34mremove - remove an element from the list.\ninsert - place a new element in the list\nsort - sort the line alphabetically\ndone - done with changes\ncheck - check the current list\033[1;37m")
+                elif command.lower() == 'check':
+                    print(nevek1)
+                    time.sleep(1)
                 elif command.lower() == 'remove':
-                    quantity = input("How many parts you want to remove? ")
+                    quantity = input("\033[1;34mHow many parts you want to remove? \033[1;37m")
                     for x in range(int(quantity)):
-                        what = input("What do you want to remove? ")
+                        what = input("\033[1;33mWhat do you want to remove? \033[1;37m")
                         nevek1.remove(what.title())
+                        time.sleep(1)
+                        print("\033[1;33m"+what.title(), "is removed.\033[1;37m")
+                        time.sleep(1)
                 elif command.lower() == 'insert':
-                    quantity = input("How many things you want to insert? ")
+                    quantity = input("\033[1;34mHow many things you want to insert? \033[1;37m")
                     for x in range(int(quantity)):
-                        where = input("Where do you want to insert? ")
-                        what = input("What do you want to insert? ")
+                        where = int(input("\033[1;33mWhere do you want to insert? \033[1;37m"))
+                        what = input("\033[1;33mWhat do you want to insert? \033[1;37m")
                         where -= 1
-                        nevek1.insert(int(where), what)
+                        time.sleep(1)
+                        nevek1.insert(int(where), what.title())
+                        print("\033[1;33m"+what.title(), "is added.\033[1;37m")
+                        time.sleep(1)
                 elif command.lower() == 'sort':
                     nevek1.sort()
+                    time.sleep(1)
+                    print("\033[1;34mList is sorted.\033[1;37m")
                 elif command.lower() == 'done':
-                    print("No other changes will be made.")
+                    print("\033[1;34mNo other changes will be made.\033[1;37m")
                 else:
-                    print("You mistyped something.")
+                    print("\033[1;31mYou mistyped something.\033[1;37m")
         else:
-            print("You mistyped something.")
+            print("\033[1;31mYou mistyped something.\033[1;37m")
     elif check.lower() == "no":
         passing = True
-        print("Passing without checking...")
+        print("\033[1;33mPassing without checking...\033[1;37m")
     else:
-        print("You typed something wrong.")
+        print("\033[1;31mYou typed something wrong.\033[1;37m")
+
+print("\033[1;31mCounting in progress...\033[1;37m")
+
+aa = 0
+
+while int(aa) != 3:
+    aa +=1
+    print(str(aa) + '...')
+    time.sleep(1)
+
+
+nevek2=[]
+lepes=0
+
+for i in range(len(nevek1)):
+    nevek2.append(nevek1[lepes])
+    lepes += 1
+    for j in range(5):
+        nevek2.append(random.randint(1, 5))
+print(nevek2)
+
+
+
+
+
+
+
+
+
+
+
 
 
